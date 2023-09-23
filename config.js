e8c5e5e3b9186e43780a9a9bafc5590b1f35dc16
Graham Wakefield <grrrwaaa@gmail.com>
2023-09-23 16:45:44 -0400

    config

---
+++ config.js
@@ -0,0 +1,36 @@
+const assert = require("assert"),
+fs = require("fs"),
+path = require("path");
+
+/*
+    Basic idea here is that we have a configuration defined in a JSON file
+
+
+*/
+
+function Config(filepath, config={}) {
+
+    let lasttime
+
+    function load() {
+        lasttime = fs.statSync(filepath).mtimeMs
+        try {
+            Object.assign(config, JSON.parse(fs.readFileSync(filepath)))
+        } catch (e) {
+            console.error("JSON ERROR", e)
+        }
+    }
+
+    load()
+
+    // now also watch the file:
+    let watcher = fs.watch(path, (eventType, filename) => {
+        if (fs.statSync(filepath).mtimeMs > lasttime) load()
+    })
+
+    return config
+}
+
+module.exports = {
+    Config
+}
