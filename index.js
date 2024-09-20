024e8784b5d818f551f59f97175022240e9d50cf
grrrwaaa <grrrwaaa@gmail.com>
2024-09-20 13:59:47 -0400

    index

---
+++ index.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    gl: require('./gles3.js'),
+    glfw: require('./glfw3.js'),
+	glutils: require('./glutils.js'),
+
+
+    Window: require("./window.js"),
+	Shaderman: require('./shaderman.js'),
+    Prng: require("./prng.js"),
+}
