791389087f3a195dbf7d5cf621ff5c1765382fdb
grrrwaaa <grrrwaaa@gmail.com>
2025-05-15 23:07:35 -0400

    displays

--- shaders/monitor.frag.glsl
+++ shaders/monitor.frag.glsl
@@ -1,14 +1,36 @@
 #version 330
 precision mediump float;
 
+uniform vec4 crop;
+uniform vec4 blend;
+
+uniform vec2 resolution;
+uniform vec2 grid_origin;
+uniform vec2 grid_scale;
 uniform sampler2D u_tex;
 
 in vec2 v_uv;
+in vec2 v_coord;
 
 layout(location = 0) out vec4 out_color;
 
+float max2 (vec2 v) {
+  return max (v.x, v.y);
+}
+
 void main() {
-    out_color = vec4(v_uv, 0, 1);
+
+    float aspect = resolution.x / resolution.y;
+
+    vec2 pixel = v_coord * resolution;
+
+    out_color = vec4(v_coord, 0, 1);
+
+    // edge blending
+    out_color  *= smoothstep(crop.x, crop.x+blend.x, pixel.x);
+    out_color  *= 1-smoothstep(crop.z + blend.z, crop.z, resolution.x-pixel.x-1);
+    out_color  *= smoothstep(crop.y, crop.y+blend.y, pixel.y);
+    out_color  *= 1-smoothstep(crop.w+blend.w, crop.w, resolution.y-pixel.y-1);
 
     if (true) {
         
@@ -20,11 +42,28 @@ void main() {
         
         // gridlines:
         //vec2 p = v_uv;
-        vec2 p = abs(0.5-mod((v_uv * 8. + 0.5), 1.));
+        vec2 p = abs(0.5-mod((v_uv * resolution/grid_scale + 0.5), 1.));
         vec2 p2 = exp(-100.*p);
         //out_color = mix(out_color, vec4(1), 0.5*length(p2));
 
-        out_color.rgb += vec3(0.5, 0.5, 1) * length(p2);
+        //out_color.rgb += vec3(0.5, 0.5, 1) * length(p2);
+
+        // origin line:
+
+        out_color.rgb += vec3(max2(exp(-0.5*abs(pixel - grid_origin))));
+
+        vec2 grid1 = mod(pixel - grid_origin, grid_scale.x);
+        out_color.rgb += vec3(max2(exp(-3.*abs(grid1))));
+
+        
+        vec2 grid2 = mod(pixel - grid_origin, grid_scale.y);
+        out_color.rgb += vec3(max2(exp(-1*abs(grid2))));
 
     }
+
+    // edge masking
+    out_color  *= step(crop.x, pixel.x);
+    out_color  *= step(crop.z, resolution.x-1-pixel.x);
+    out_color  *= step(crop.y, pixel.y);
+    out_color  *= step(crop.w, resolution.y-1-pixel.y);
 }
